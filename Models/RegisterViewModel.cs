0720009a69d5717e389126ae7c099544bf726ab0
TrisLee <135494129+Backtodagame@users.noreply.github.com>
2025-11-13 09:53:14 +0700

    Add RegisterViewModel for user registration

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+//Trí tạo View đăng ký
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models // Hoặc WebApplication1.ViewModels
+{
+
+    public class RegisterViewModel 
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Xác nhận mật khẩu")]
+        [Compare("Password", ErrorMessage = "Mật khẩu và xác nhận mật khẩu không khớp.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
